bce8711ad80b838d65b662ed03419e634fdda430
LanderVerschueren <verschueren@live.nl>
2016-10-13 14:31:59 +0200

    nav

--- Site/resources/views/includes/nav3.blade.php
+++ Site/resources/views/includes/nav3.blade.php
@@ -1,1 +1,32 @@
-dfqds
+<div class="navigation">
+	<div class="user-info">
+		@if (Auth::guest())
+		<li><a href="{{ url('/login') }}">Inloggen</a></li>
+		<li class=""><a href="{{ url('/register') }}">Registreren</a></li>
+		@else
+		<li class="dropdown">
+			<a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">
+				{{ Auth::user()->name }} <span class="caret"></span>
+			</a>
+
+			<ul class="dropdown-menu" role="menu">
+				<li>
+					<a href="{{ url('/logout') }}"
+					onclick="event.preventDefault();
+					document.getElementById('logout-form').submit();">
+					Logout
+					</a>
+
+					<form id="logout-form" action="{{ url('/logout') }}" method="POST" style="display: none;">
+						{{ csrf_field() }}
+					</form>
+				</li>
+			</ul>
+		</li>
+		@endif
+	</div>
+
+	<div class="rest-info">
+		
+	</div>
+</div>
