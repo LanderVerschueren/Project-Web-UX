0678079640a8b12432474f9318833448e0bad138
thomas van roy <thomsvanroy08@gmail.com>
2016-10-20 10:59:55 +0200

    nieuw.blade aangepast

--- Site/resources/views/pages/nieuw.blade.php
+++ Site/resources/views/pages/nieuw.blade.php
@@ -6,7 +6,40 @@
             <div class="col-md-12">
                 <div class="panel panel-default">
                     <div class="panel-body">
-                        create new offer here
+                        <form class="form-horizontal" role="form" method="POST" action="{{ url('/') }}">
+                            {{ csrf_field() }}
+
+                            <div class="form-group">
+                                <label for="naam" class="col-md-4 control-label">naam</label>
+
+                                <div class="col-md-6">
+                                    <input id="naam" type="text" class="form-control" name="naam" value="{{ old('naam') }}" required autofocus>
+                                </div>
+                            </div>
+
+                            <div class="form-group">
+                                <label for="aantal" class="col-md-4 control-label">aantal</label>
+
+                                <div class="col-md-6">
+                                    <input id="aantal" type="integer" class="form-control" name="aantal" value="{{ old('aantal') }}" required autofocus>
+                                </div>
+                            </div>
+
+                            <div class="form-group">
+                                <label for="prijs" class="col-md-4 control-label">prijs</label>
+
+                                <div class="col-md-6">
+                                    <input id="prijs" type="integer" class="form-control" name="prijs" value="{{ old('prijs') }}" required>
+                                </div>
+                            </div>
+
+                            <div class="form-group">
+                                <div class="col-md-6 col-md-offset-4">
+                                    <button type="submit" class="btn btn-primary">
+                                        make offer
+                                    </button>
+                                </div>
+                            </div>
                     </div>
                 </div>
             </div>
