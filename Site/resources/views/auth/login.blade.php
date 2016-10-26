3303dbb79eab08b08113a37f0e7a642e41f013b5
LanderVerschueren <verschueren@live.nl>
2016-10-26 12:28:18 +0200

    login

--- Site/resources/views/auth/login.blade.php
+++ Site/resources/views/auth/login.blade.php
@@ -1,63 +1,8 @@
-@section('test')
-   <div class="modal-dialog">
-        <div class="modal-content">
-            <div class="modal-header">
-                <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">×</span></button>
-                <h4 class="modal-title" id="myModalLabel">Register</h4>
-            </div>
-            <div class="modal-body">
- 
-                <form id="formRegister" class="form-horizontal" role="form" method="POST" action="{{ url('/auth/register') }}">
-                    <input type="hidden" name="_token" value="{{ csrf_token() }}">
- 
-                    <div class="form-group">
-                        <label class="col-md-4 control-label">Name</label>
-                        <div class="col-md-6">
-                            <input type="text" class="form-control" name="name">
-                            <small class="help-block"></small>
-                        </div>
-                    </div>
- 
-                    <div class="form-group">
-                        <label class="col-md-4 control-label">E-Mail Address</label>
-                        <div class="col-md-6">
-                            <input type="email" class="form-control" name="email">
-                            <small class="help-block"></small>
-                        </div>
-                    </div>
- 
-                    <div class="form-group">
-                        <label class="col-md-4 control-label">Password</label>
-                        <div class="col-md-6">
-                            <input type="password" class="form-control" name="password">
-                            <small class="help-block"></small>
-                        </div>
-                    </div>
- 
-                    <div class="form-group">
-                        <label class="col-md-4 control-label">Confirm Password</label>
-                        <div class="col-md-6">
-                            <input type="password" class="form-control" name="password_confirmation">
-                        </div>
-                    </div>
- 
-                    <div class="form-group">
-                        <div class="col-md-6 col-md-offset-4">
-                            <button type="submit" class="btn btn-primary">
-                                Register
-                            </button>
-                        </div>
-                    </div>
-                </form>                       
- 
-            </div>
-        </div>
-    </div>
+@extends('layouts.app')
 
-  @endsection
+@section('title', 'Inloggen')
 
-
-<!--
+@section('content')
 <div class="container">
     <div class="row">
         <div class="col-md-8 col-md-offset-2">
@@ -112,4 +57,4 @@
         </div>
     </div>
 </div>
--->
+@endsection
