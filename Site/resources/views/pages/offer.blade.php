74f0281af102907318babbdd6cbfc73696b65cd9
LanderVerschueren <verschueren@live.nl>
2016-10-26 16:52:24 +0200

    alles wa nog niet gepushed is

--- Site/resources/views/pages/offer.blade.php
+++ Site/resources/views/pages/offer.blade.php
@@ -3,39 +3,35 @@
 @section('title', 'Aanbieden')
 
 @section('content')
-    <div class="container">
-        <div class="row">
-            <div class="col-md-12">
-                <div class="panel panel-default">
-                    <div class="panel-body">
-                        @if($offers->count() > 0)
-                            @if($offers->where('user_id', Auth::user()->id)->count() > 0)
-                                <h1>Jou toegevoegde offers</h1>
-                                <table id="offers">
-                                    @foreach($offers as $offer)
-                                        @if($offer->user_id == Auth::user()->id)
-                                        <tr>
-                                            <td>{{ $offer->naam }}</td>
-                                            <td>{{ $offer->aantal }}</td>
-                                            <td>{{ $offer->prijs }}</td>
-                                        </tr>
-                                        @endif
-                                    @endforeach
-                                </table>
-                            @else
-                                <h1>Je hebt nog geen aanbiedingen toegevoegd</h1>
-                            @endif
+<div class="container_column">
+    @if($offers->count() > 0)
+        @if($offers->where('user_id', Auth::user()->id)->count() > 0)
+            <h2>Jou toegevoegde offers</h2>
 
-                            <br>
-                            <a href="\nieuw">voeg een nieuw offer toe</a>
-                        @else
-                            <h1>Je hebt nog geen aanbiedingen toegevoegd</h1>
-
-                            <a href="\nieuw">voeg een nieuw offer toe</a>
-                        @endif
+            @foreach($offers as $offer)
+                @if($offer->user_id == Auth::user()->id)
+                <div class="offer container_column panel panel-default">
+                    <div class="title_offer panel-heading">
+                        <h2>{{ ucfirst($offer->naam) }}</h2>
                     </div>
-                </div>
-            </div>
-        </div>
-    </div>
+                    <div class="info_offer container_row panel-body">
+                        <div class="left">
+                            <img src="images/{{ $offer->foto }}" alt="">
+                        </div>
+                        <div class="right">
+                            <span>prijs: {{ $offer->prijs }} euro</span>
+                            <span>aantal: {{ $offer->aantal }}</span>
+                        </div>
+                    </div>
+                    <div class="panel-footer">
+                        <a href="">Pas aanbieding aan</a>
+                    </div>
+                </div>   
+                @endif 
+            @endforeach
+        @else
+            <h1>Er zijn nog geen aanbiedingen toegevoegd</h1>
+        @endif
+    @endif
+</div>
 @endsection
