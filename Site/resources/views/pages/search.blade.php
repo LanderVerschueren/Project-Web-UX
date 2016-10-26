170376720f91282a8a81c452c22e9fbb0bc1d1ae
thomas van roy <thomsvanroy08@gmail.com>
2016-10-26 17:10:00 +0200

    verbeterde versie code

--- Site/resources/views/pages/search.blade.php
+++ Site/resources/views/pages/search.blade.php
@@ -13,13 +13,13 @@
                             @foreach($searchResults as $result)
                                 <div class="offerpanel col-md-6">
                                     <div class="col-md-8 offerpanel-image-body">
-                                        <img class="offerpanel-image" src="images/{{ $result->foto }}">
+                                        <img class="offerpanel-image temporary" src="images/{{ $result->foto }}">
                                     </div>
                                     <div class="col-md-4 offerpanel-info">
                                         <h2>{{ $result->naam }}</h2>
                                         <p>prijs: {{ $result->prijs }} euro</p>
                                         <p>aantal: {{ $result->aantal }}</p>
-                                        <a href="user/{id}">{{ \App\User::find($result->user_id)->achternaam }} {{ \App\User::find($result->user_id)->voornaam }}</a>
+                                        <a href="user/{id}">{{ $users->find($result->user_id)->achternaam . ' ' . $users->find($result->user_id)->voornaam }}</a>
                                     </div>
                                 </div>
                             @endforeach
