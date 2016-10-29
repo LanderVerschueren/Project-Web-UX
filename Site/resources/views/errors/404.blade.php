b93f6aa10f5ee5f925698ae7b1e1164dd8b1b426
thomas van roy <thomsvanroy08@gmail.com>
2016-10-29 07:59:33 +0200

    404 page added

---
+++ Site/resources/views/errors/404.blade.php
@@ -0,0 +1,9 @@
+@extends('layouts.app')
+
+@section('title', 'Page not found')
+
+@section('content')
+    <div class="container_column buy_page">
+        this page was not found, nice try though.
+    </div>
+@endsection
