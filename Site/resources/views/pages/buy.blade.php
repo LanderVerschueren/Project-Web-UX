fb68568111befe2e5c8635dd96bb30d1d62e0f4c
LanderVerschueren <verschueren@live.nl>
2016-11-03 17:09:14 +0100

    Changed colors

--- Site/resources/views/pages/buy.blade.php
+++ Site/resources/views/pages/buy.blade.php
@@ -3,26 +3,34 @@
 @section('title', 'Kopen')
 
 @section('content')
-<div class="container_column buy_page">
+<div class="container_row buy_page">
     @if($offers->count() > 0)
         @foreach($offers as $offer)
-        <div class="card"></div>
-        <div class="card offer">
-            <h1 class="title">{{ ucfirst($offer->naam) }}</h1>
-            <div class="info_offer container_row">
-                <div class="left">
-                    <img src="images/{{ $offer->foto }}" alt="">
-                </div>
-                <div class="right">
-                    <span>prijs: {{ $offer->prijs }} euro</span>
-                    <span>aantal: {{ $offer->aantal }}</span>
-                    <span><a href="user/{id}">{{ ucfirst($users->where('id',$offer->user_id)->first()->voornaam) . " " . ucfirst($users->where('id',$offer->user_id)->first()->achternaam) }}</a></span>
-                </div>
+        <div class="col-sm-6">
+            <div class="container_card">
+                <div class="card"></div>
+                <div class="card offer">
+                    <h1 class="title">{{ ucfirst($offer->naam) }}</h1>
+                    <div class="info_offer container_row">
+                        <div class="left">
+                            <img src="images/{{ $offer->foto }}" alt="">
+                        </div>
+                        <div class="right">
+                            <span>prijs: {{ $offer->prijs }} euro</span>
+                            <span>aantal: {{ $offer->aantal }}</span>
+                            <span><a href="user/{id}">{{ ucfirst($users->where('id',$offer->user_id)->first()->voornaam) . " " . ucfirst($users->where('id',$offer->user_id)->first()->achternaam) }}</a></span>
+                        </div>
+                    </div>
+                    <div class="button-container">
+                        <div class="container_row">
+                            <div class="button">
+                                <a href="/offer/{{ $offer->id }}">Bekijk aanbieding</a>
+                            </div>
+                        </div>
+                    </div>
+                </div>  
             </div>
-            <div class="button-container">
-              <a href="/offer/{{ $offer->id }}">Bekijk aanbieding</a>
-            </div>
-        </div>   
+        </div> 
         @endforeach
     @else
         <h1>Er zijn nog geen aanbiedingen toegevoegd</h1>
--- Site/resources/views/pages/offer.blade.php
+++ Site/resources/views/pages/offer.blade.php
@@ -1,42 +1,55 @@
 @extends('layouts.app')
 
-@section('title', 'Aanbieden')
+@section('title', 'Aanbieden - Jouw toegevoegde offers')
 
 @section('content')
 <div class="container_column offer_page">
     @if($offers->count() > 0)
         @if($offers->where('user_id', Auth::user()->id)->count() > 0)
-            <h2>Jouw toegevoegde offers</h2>
+            <div class="button-container offer-container">
+                <div class="container_row">
+                    <div class="button">
+                        <a href="/nieuw">Post een nieuwe aanbieding</a>
+                    </div>                    
+
+                </div>
+            </div>
+            
 
             @foreach($offers as $offer)
                 @if($offer->user_id == Auth::user()->id)
-                <div class="card"></div>
-                <div class="card offer">
-                    <h1 class="title">{{ ucfirst($offer->naam) }}</h1>
-                    <div class="info_offer container_row">
-                        <div class="left">
-                            <img src="images/{{ $offer->foto }}" alt="">
-                        </div>
-                        <div class="right">
-                            <span>prijs: {{ $offer->prijs }} euro</span>
-                            <span>aantal: {{ $offer->aantal }}</span>
-                            <span></span>
-                        </div>
-                    </div>
-                    <div class="button-container">
-                        <div class="container_row">
-                            <div class="button">
-                                <a href="/offer/{{ $offer->id }}">Bekijk aanbieding</a>
-                            </div>
-                        </div>
-                    </div>
-                </div>     
+                <div class="container_row">
+                    <div class="col-sm-6">
+                        <div class="container_card">
+                            <div class="card"></div>
+                            <div class="card offer">
+                                <h1 class="title">{{ ucfirst($offer->naam) }}</h1>
+                                <div class="info_offer container_row">
+                                    <div class="left">
+                                        <img src="images/{{ $offer->foto }}" alt="">
+                                    </div>
+                                    <div class="right">
+                                        <span>prijs: {{ $offer->prijs }} euro</span>
+                                        <span>aantal: {{ $offer->aantal }}</span>
+                                        <span></span>
+                                    </div>
+                                </div>
+                                <div class="button-container">
+                                    <div class="container_row">
+                                        <div class="button">
+                                            <a href="/offer/{{ $offer->id }}">Bekijk aanbieding</a>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div> 
+                        </div> 
+                    </div>  
+                </div> 
                 @endif 
             @endforeach
         @else
             <h1>Er zijn nog geen aanbiedingen toegevoegd</h1>
         @endif
     @endif
-    <a href="/nieuw">Post nieuwe aanbieding</a>
 </div>
 @endsection
